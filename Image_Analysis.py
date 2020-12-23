18cce25079e5bdce3ca3f28d6967e288e8098775
Isaac <isaac@atomionics.com>
2020-12-23 16:47:40 +0800

    Changed CSV saving and tidied up stuff

--- Image_Analysis.py
+++ Image_Analysis.py
@@ -23,18 +23,20 @@ def gaus(x,b,a,x0,sigma):
     return b+a*np.exp(-(x-x0)**2/(2*sigma**2))
 
 
-''' Set parameters.'''
-image_no = '00360' #Unique Image number you want to analyse. Make sure the date is today's date.
-
-num_run = 1 #number of run
+#%%Set parameters
+image_no = '00360' #Unique Image number you want to analyse. Make sure the date on the file's name is today's date.
+num_run = 1 #number of runs
 
 #Mention below the starting and end points of x and Z ROI. This wil also be used to plot two red lines showing the region
-ROI_x_start = 500
-ROI_x_end = 750
-ROI_z_start = 400
-ROI_z_end = 750
+ROI_x = [450,750]
+ROI_z = [350,750]
 cbarlim=(0,1.0) #set your colour bar limit for scaled image
 
+#%%
+ROI_x_start = ROI_x[0]
+ROI_x_end = ROI_x[1]
+ROI_z_start = ROI_z[0]
+ROI_z_end = ROI_z[1]
 ROI_x_size = ROI_x_end-ROI_x_start
 ROI_z_size = ROI_z_end-ROI_z_start
 
@@ -101,18 +103,15 @@ else:
     # lastImageNum = float(lastImageName[15:20]) + 1
     
 ''' Saving images. '''
-def plot_im():
+def proc_im():
     img_at = cv2.imread(savepath + folder_date + "-img_%05s_a.png" % (image_no),0) # With atom                    
     img_las = cv2.imread(savepath + folder_date + "-img_%05s_b.png" % (image_no),0) # Laser alone
     img_bck = cv2.imread(savepath + folder_date + "-img_%05s_c.png" % (image_no),0) # Background
-    # img_at = cv2.imread('image_a.png',0) # With atom                    
-    # img_las = cv2.imread('image_b.png',0) # Laser alone
-    # img_bck = cv2.imread('image_c.png',0) # Background
+
             
-        #%% Creation of the 2D array of optical depths
-        
-        # Substraction of the background
-        
+#%% Creation of the 2D array of optical depths
+
+# Substraction of the background
     img_las = img_las - img_bck
     img_at = img_at -img_bck
     ###Correction of the zeros
@@ -139,71 +138,54 @@ def plot_im():
             
     #    with np.printoptions(threshold=np.inf):
       
-    #Plot OD on 2,2,1
-    fig = plt.figure(figsize=(15,10))
-    plt.subplot(2,3,5)
-    plt.title("Colour Unscaled")
-    plt.xlabel("Pixels")
-    plt.ylabel("Pixels")
 
-       #plt.title("OD plot - full scale")
-       # plt.xlabel("x")
-       #plt.ylabel("OD")
-    plt.imshow(OD)
-       # plt.clim(0,1) # colourbar limit (,)
-    plt.colorbar()
       
-       #%% Fitting along the x axis (integrated over all z values)
-       # Gaussian fitting to get the width
+    #%% Fitting along the x axis (integrated over all z values)
+    # Gaussian fitting to get the width
     sum_x = np.sum(OD, axis = 0)
     n = len(sum_x)
     x = np.linspace(1,n,n)
     x_val_to_find = imin + (np.max(sum_x)-imin)*1/np.exp(1)
     a_x = np.where(x>=x_val_to_find)
-    
     if np.max(sum_x)<=0:
         in_max_x = n/2
         print("np.max(sum_x) is negative!")
     else:
         in_max_x = np.where(x==round(np.max(sum_x)))[0][0]
     fit_x,pcov = curve_fit(gaus,x,sum_x,p0=[imin,np.max(sum_x),in_max_x,len(a_x[0])/2])
-        # Measure of the fitting accuracy for the horizontal cross-section
-
-
+    # Measure of the fitting accuracy for the horizontal cross-section
     err_x = sqrt(abs(pcov[3,3]))/fit_x[3]
     print("OD x  fit done")       
-       #%% Fitting along the z axis (integrated over all x values)
-       # Gaussian fitting to get the width
+    #%% Fitting along the z axis (integrated over all x values)
+    # Gaussian fitting to get the width
     sum_z = np.sum(OD, axis = 1)
     n = len(sum_z)
     z = np.linspace(1,n,n)
     z_val_to_find = imin + (np.max(sum_z)-imin)*1/np.exp(1)
     a_z = np.where(z>=z_val_to_find)
-
-    
     if np.max(sum_z)<=0:
         in_max_z = n/2
         print("np.max(sum_z) is negative!")
     else:
         in_max_z = np.where(z==round(np.max(sum_z)))[0][0]
     fit_z,pcov = curve_fit(gaus,z,sum_z,p0=[imin,np.max(sum_z),in_max_z,len(a_z[0])/2])
-       # Measure of the fitting accuracy for the vertical cross-section
+    # Measure of the fitting accuracy for the vertical cross-section
     err_z = sqrt(abs(pcov[3,3]))/fit_z[3]
     print("OD z  fit done")  
-       #%% Global peak optical depths
+    #%% Global peak optical depths
       
     center_x = int(round(fit_x[2]))
     center_z = int(round(fit_z[2]))
     center_x = ROI_x_start + int(ROI_x_size/2)
     center_z = ROI_z_start + int(ROI_z_size/2)
-    #    # Cross sections centered in the previous peaks
+    # Cross sections centered in the previous peaks
     cross_x1 = OD[center_z,:]
     cross_x = cross_x1[ROI_x_start:ROI_x_end]
     cross_z1 = OD[:,center_x]
     cross_z = cross_z1[ROI_z_start:ROI_z_end];
     x = np.linspace(1,len(cross_x), len(cross_x))
     z = np.linspace(1,len(cross_z), len(cross_z))
-       # Gaussian fitting of those cross sections
+    # Gaussian fitting of those cross sections
     cross_x_fit,pcov = curve_fit(gaus,x,cross_x,p0=[imin, np.max(cross_x),len(cross_x)/2,1])
     cross_z_fit,pcov = curve_fit(gaus,z,cross_z,p0=[imin, np.max(cross_z),len(cross_z)/2,1])
     print("OD second  fit done")  
@@ -217,26 +199,35 @@ def plot_im():
     points_z = [ROI_z_start+(ROI_z_size/2), ROI_z_start+(ROI_z_size/2)]
     points_z2 = [ROI_z_start, ROI_z_end]
     points_x2= [ROI_x_start + (ROI_x_size/2), ROI_x_start + (ROI_x_size/2)]
-    
+        
+    #%%Plotting
+    fig = plt.figure(figsize=(15,10))
+    plt.subplot(2,3,5)
+    plt.title("Colour Unscaled")
+    plt.xlabel("Pixels")
+    plt.ylabel("Pixels")
+    plt.imshow(OD)
+    plt.colorbar()
     fit_xl = gaus(x, *cross_x_fit)
     plt.subplot(2,3,4)
     plt.title("Fitting on x-axis")
     plt.xlabel("Pixels")
+    plt.ylabel("OD")
     xpixels=np.linspace(ROI_x_start,ROI_x_end,ROI_x_size)
-    plt.plot(xpixels,cross_x,'--')
-    plt.plot(xpixels,fit_xl,'--')
+    plt.plot(xpixels,cross_x,'--b')
+    plt.plot(xpixels,fit_xl,'--r')
       
     fit_zl = gaus(z, *cross_z_fit)
     plt.subplot(2,3,2)
     plt.xlabel("Pixels")
+    plt.ylabel("OD")
     plt.title("Fitting on z-axis")
     zpixels=np.linspace(ROI_z_start,ROI_z_end,ROI_z_size)
-    plt.plot(zpixels,cross_z,'--')
-    plt.plot(zpixels,fit_zl,'--')
+    plt.plot(zpixels,cross_z,'--b')
+    plt.plot(zpixels,fit_zl,'--r')
      
     plt.subplot(2,3,1)
     plt.title("Colour Scaled \n OD ROI_Sum = %i"  %ROI_sum,  fontsize=12)
-    #plt.ylabel("OD")
     plt.xlabel("Pixels")
     plt.ylabel("Pixels")
     plt.plot(points_x, points_z, linestyle='dashed',linewidth=0.5, color='red')
@@ -268,19 +259,19 @@ def plot_im():
     areaz = np.sum(sum_z-imin)
     Nx = areax*px_size**2/sigmatotal
     Nz = areaz*px_size**2/sigmatotal
-
-    
+    Npxsum = ROI_sum*px_size**2/sigmatotal
 
     plt.subplot(2,3,3)
-    plt.text(0, 0.8, "Date: " +folder_date)
-    plt.text(0, 0.85, "Image #: %05s" % (image_no))
-    plt.text(0, 0.6, "Important Parameters:")
-    plt.text(0, 0.55, "OD = " + str(round(ODpk,3)))
-    plt.text(0, 0.50, "$\sigma_{x}$ = " + str(round(sigma_x,6)*10**6)+"mm")
-    plt.text(0, 0.45, "$\sigma_{z}$ = " + str(round(sigma_z,6)*10**6)+"mm")
-    plt.text(0, 0.40, "$N_{OD}$ = " + str(round(N_OD/(10**6),1)) + "*$10^{6}$ atoms") 
-    plt.text(0, 0.35, "$N_{x}$ =  " + str(round(Nx/(10**6),1)) + "$*10^{6}$ atoms")
-    plt.text(0, 0.30, "$N_{z}$ =  " + str(round(Nz/(10**6),1)) + "$*10^{6}$ atoms")
+    plt.text(-0.1, 0.9, "Image #: %05s" % (image_no), fontsize=30)
+    plt.text(-0.1, 0.8, "Date: " +folder_date, fontsize=30)
+    plt.text(-0.1, 0.6, "Important Parameters", fontsize=20, )
+    plt.text(-0.1, 0.50, "Peak OD = " + str(round(ODpk,3)), fontsize=15)
+    plt.text(-0.1, 0.40, "$\sigma_{x}$ = " + str(round(sigma_x,6)*10**6)+"mm", fontsize=15)
+    plt.text(-0.1, 0.30, "$\sigma_{z}$ = " + str(round(sigma_z,6)*10**6)+"mm", fontsize=15)
+    plt.text(-0.1, 0.20, "$N_{OD}$ = " + str(round(N_OD/(10**6),1)) + "*$10^{6}$ atoms", fontsize=15) 
+    plt.text(-0.1, 0.10, "$N_{x}$ =  " + str(round(Nx/(10**6),1)) + "$*10^{6}$ atoms", fontsize=15)
+    plt.text(-0.1, 0.00, "$N_{z}$ =  " + str(round(Nz/(10**6),1)) + "$*10^{6}$ atoms", fontsize=15)
+    plt.text(-0.1, -0.10, "$N_{px sum}$ =  " + str(round(Npxsum/(10**6),1)) + "$*10^{6}$ atoms", fontsize=15)
     plt.axis('off')
                
        #save image
@@ -291,7 +282,7 @@ def plot_im():
 
       #%% Export parameters of interest
      
-    headline = ['Run #', 'filename','sigma_x','sigma_z','ROI_sum', 'sigma_x fit error','sigma_z fit error']
+    headline = ['Image #', 'filename','Placeholder','sigma_x','sigma_z','X Pos','Z Pos',"N_OD","N_x","N_z","N_pxsum0","Peak OD"]
       # If the csv file does not exist yet, creates it with its header
     if not os.path.exists(savepath + folder_date + "-Data00000.csv"):
         with open(savepath + folder_date +  '-Data00000.csv', 'x', newline = '') as file:
@@ -302,10 +293,12 @@ def plot_im():
         reader = csv.reader(file, dialect = 'excel')
         rows = list(reader)
         line_num = len(rows)
-    parameters = [line_num,filename, sigma_x,sigma_z, ROI_sum, err_x,err_z]
+    parameters = [image_no,filename,'Nan', sigma_x,sigma_z,str(ROI_x[0])+"-"+str(ROI_x[1]),str(ROI_z[0])+"-"+str(ROI_z[1]), N_OD, Nx, Nz, Npxsum, ODpk]
       # Adds the new set of parameters following the existing lines
     with open(savepath + folder_date + '-Data00000.csv', 'a', newline = '') as file:
         writer = csv.writer(file, dialect = 'excel', quoting = csv.QUOTE_NONE)
         writer.writerow(parameters)
+
+
 for i in range(num_run):
-    plot_im()
+    proc_im()
